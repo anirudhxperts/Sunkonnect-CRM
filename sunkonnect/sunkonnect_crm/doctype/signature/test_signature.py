a96d14fd05ae89ba85aeaa112c6a5aa6717bcb64
anirudhxperts <anirudhra@unifyxperts.com>
2025-01-31 08:56:37 +0000

    Production setup

---
+++ sunkonnect/sunkonnect_crm/doctype/signature/test_signature.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, manan shah and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSignature(FrappeTestCase):
+	pass
